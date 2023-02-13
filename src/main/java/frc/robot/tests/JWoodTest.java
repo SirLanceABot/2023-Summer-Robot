d7ec41f30d669a7bbe7505a4a5e741df9a1b7749
jwoodard <jwoodard@lpslancer.net>
2023-02-12 20:24:26 -0500

    jw - halftime adjustments

--- src/main/java/frc/robot/tests/JWoodTest.java
+++ src/main/java/frc/robot/tests/JWoodTest.java
@@ -2,9 +2,12 @@ package frc.robot.tests;
 
 import java.lang.invoke.MethodHandles;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.Shoulder;
 
 public class JWoodTest implements Test
 {
@@ -21,15 +24,17 @@ public class JWoodTest implements Test
     // *** CLASS & INSTANCE VARIABLES ***
     // Put all class and instance variables here.
     private final RobotContainer robotContainer;
+    PowerDistribution pdp = new PowerDistribution();
+    private final CANSparkMax motor = new CANSparkMax(3,  MotorType.kBrushless); 
     private final Joystick joystick = new Joystick(0);
-    private final Shoulder shoulder;
 
 
     // *** CLASS CONSTRUCTOR ***
     public JWoodTest(RobotContainer robotContainer)
     {
         this.robotContainer = robotContainer;
-        this.shoulder = this.robotContainer.shoulder;
+
+        motor.restoreFactoryDefaults();
 
     }
 
@@ -44,7 +49,8 @@ public class JWoodTest implements Test
      */
     public void periodic()
     {
-        shoulder.on(0.75);
+        motor.set(0.5);
+        System.out.println(pdp.getTotalEnergy() + " " + pdp.getCurrent(14) + " " + motor.getOutputCurrent());
     }
     
     /**
@@ -52,7 +58,7 @@ public class JWoodTest implements Test
      */
     public void exit()
     {
-        shoulder.off();
+        motor.set(0.0);
     }
 
     // *** METHODS ***
