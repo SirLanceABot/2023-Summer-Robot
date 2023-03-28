953c1047fd011cbbb1b9d239e692c969f6bbed49
Team 4237 Laptop 1 <LakeshoreRoboticsTeam@gmail.com>
2023-03-27 20:28:56 -0400

    OD LB worked on sonar

--- src/main/java/frc/robot/tests/OwenTest.java
+++ src/main/java/frc/robot/tests/OwenTest.java
@@ -1,6 +1,7 @@
 package frc.robot.tests;
 
 import java.lang.invoke.MethodHandles;
+import java.lang.management.OperatingSystemMXBean;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
@@ -11,11 +12,19 @@ import frc.robot.RobotContainer;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Grabber;
 import frc.robot.subsystems.Shoulder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.commands.GrabGamePiece;
 import frc.robot.commands.SuctionControl;
 import frc.robot.shuffleboard.AutonomousTab;
 import frc.robot.shuffleboard.MainShuffleboard;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import frc.robot.controls.Xbox;
+import frc.robot.controls.OperatorController;
 
 public class OwenTest implements Test
 {
@@ -34,8 +43,24 @@ public class OwenTest implements Test
     private final RobotContainer robotContainer;
     // private final CANSparkMax grabberMotor = new CANSparkMax(3, MotorType.kBrushless);
     private final Joystick joystick = new Joystick(0);
+    private final double rumbleDistance = 1.0e3;
+    private final double kP = -0.001;
+    private final double kI = 0.0;
+    private final double kD = 0.0;
+    final int kUltrasonicPingPort = 2;
+    final int kUltrasonicEchoPort = 1;
+    private final MedianFilter m_filter = new MedianFilter(5);
+    // private final PWMSparkMax m_leftMotor = new PWMSparkMax(kUltrasonicEchoPort);
+    // private final Ultrasonic m_ultrasonic1 = new Ultrasonic(kUltrasonicPingPort, kUltrasonicEchoPort);
+    // private final Ultrasonic m_ultrasonic3 = new Ultrasonic(null, null);
+    // private final Ultrasonic m_ultrasonic2 = new Ultrasonic(kUltrasonicPingPort, kUltrasonicPingPort);
+    // private final PIDController m_pidController = new PIDController(kP, kI, kD);
+    private final AnalogInput sonarSensor = new AnalogInput(0);
+    private final AnalogPotentiometer pot = new AnalogPotentiometer(sonarSensor, 5000, 0);
+    // sonarSensor.setAverageBits(2);
     // private final Grabber grabber;// = RobotContainer.grabber;
-    private final Shoulder shoulder;
+    // private final Shoulder shoulder;
+    private final OperatorController operatorController;
     // private final Arm arm;
     // private final MainShuffleboard mainShuffleboard;
     // private final CloseGrabber closeGrabber;
@@ -46,9 +71,10 @@ public class OwenTest implements Test
     public OwenTest(RobotContainer robotContainer)
     {
         this.robotContainer = robotContainer;
+        this.operatorController = robotContainer.operatorController;
         // this.grabber = robotContainer.grabber;
         // this.mainShuffleboard = robotContainer.mainShuffleboard;
-        this.shoulder = robotContainer.shoulder;
+        // this.shoulder = robotContainer.shoulder;
         // this.arm = robotContainer.arm;
     }
 
@@ -57,7 +83,7 @@ public class OwenTest implements Test
      */
     public void init()
     {
-        
+        // m_pidController.setSetpoint(rumbleDistance);
     }
 
     /**
@@ -65,31 +91,38 @@ public class OwenTest implements Test
      */
     public void periodic()
     {
-        // grabber.compressorEnable();
-        // Joystick();
-        if(joystick.getRawButton(4))
+        // double measurement = m_ultrasonic1.getRangeMM();
+        // double filteredMeasurement = m_filter.calculate(measurement);
+        // double pidOutput = m_pidController.calculate(filteredMeasurement);
+        pot.get();
+        System.out.println("Distance: " + pot.get() / 304.8 + "\n");
+        if(pot.get() / 304.8 < 4.0 && pot.get() / 304.8 > 3.0)
         {
-            System.out.println("Up");
-            shoulder.moveUp();
-            // arm.extendArm();
+            operatorController.setRumble(0.1, 0.5, 0.5);
         }
-        else if(joystick.getRawButton(3))
-        {
-            System.out.println("Down");
-            shoulder.moveDown();
-            // arm.retractArm();
-        }
-        else
-        {
-            shoulder.off();
-            // arm.stopArm();
-        }
+        // // grabber.compressorEnable();
+        // // Joystick();
+        // if(joystick.getRawButton(4))
+        // {
+        //     System.out.println("Up");
+        //     // shoulder.moveUp();
+        //     // arm.extendArm();
+        // }
+        // else if(joystick.getRawButton(3))
+        // {
+        //     System.out.println("Down");
+        //     // arm.retractArm();
+        // }
+        // // else
+        // // {
+        // //     // arm.stopArm();
+        // // }
 
-        if(joystick.getRawButton(1))
-        {
-            shoulder.resetEncoder();
-            // arm.resetEncoder();
-        }
+        // // if(joystick.getRawButton(1))
+        // // {
+        // //     shoulder.resetEncoder();
+        // //     // arm.resetEncoder();
+        // // }
     }
     
     /**
